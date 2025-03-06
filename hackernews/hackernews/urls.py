fc85042bc9467786eadb18c7b4ab00b1214b753c
Deo <okoride0@gmail.com>
2025-03-06 11:03:40 +0300

    install GraphiQL

--- hackernews/hackernews/urls.py
+++ hackernews/hackernews/urls.py
@@ -20,3 +20,11 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+from django.views.decorators.csrf import csrf_exempt
+from graphene_django.views import GraphQLView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+]
