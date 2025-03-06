59efc427b67f7e7396d7d3b326f38284b789afe4
Deo <okoride0@gmail.com>
2025-03-06 15:21:09 +0300

    add me query to test auth

--- hackernews/users/schema.py
+++ hackernews/users/schema.py
@@ -33,3 +33,16 @@ class Query(graphene.ObjectType):
 
     def resolve_users(self, info):
         return get_user_model().objects.all()
+    
+class Query(graphene.AbstractType):
+    me = graphene.Field(UserType)
+    users = graphene.List(UserType)
+
+    def resolve_users(self, info):
+        return get_user_model().objects.all()
+    
+    def resolve_me(self, info):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception('Not logged in!')
+        return user
