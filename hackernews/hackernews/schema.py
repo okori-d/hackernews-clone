362b9a35e994947f80419b2e2ebfff4d3254bea5
Deo <okoride0@gmail.com>
2025-03-06 15:03:56 +0300

    configure django-graphql-jwt

--- hackernews/hackernews/schema.py
+++ hackernews/hackernews/schema.py
@@ -2,12 +2,16 @@ import graphene
 
 import links.schema
 import users.schema
+import graphene
+import graphql_jwt
 
 
 class Query(users.schema.Query, links.schema.Query, graphene.ObjectType):
     pass
 
 class Mutation(users.schema.Mutation, links.schema.Mutation, graphene.ObjectType):
-    pass
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
--- hackernews/hackernews/settings.py
+++ hackernews/hackernews/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
 ]
 
 ROOT_URLCONF = 'hackernews.urls'
@@ -126,4 +127,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 GRAPHENE = {
     'SCHEMA': 'hackernews.schema.schema',
+    'SCHEMA': 'mysite.myschema.schema',
+    'MIDDLEWARE': [
+        'graphql_jwt.middleware.JSONWebTokenMiddleware',
+    ],
 }
+
+AUTHENTICATION_BACKENDS = [
+    'graphql_jwt.backends.JSONWebTokenBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
