40ef285cd87d48266aae1e5764191e6f7d34f78f
Deo <okoride0@gmail.com>
2025-03-07 22:39:18 +0300

    add GraphQL queries and mutations for link and vote models, including search, pagination and user authentication chcks

--- hackernews/links/schema.py
+++ hackernews/links/schema.py
@@ -2,6 +2,10 @@ import graphene
 from graphene_django import DjangoObjectType
 
 from .models import Link
+from users.schema import UserType
+from links.models import Link, Vote
+from graphql import GraphQLError
+from django.db.models import Q
 
 
 class LinkType(DjangoObjectType):
@@ -9,17 +13,43 @@ class LinkType(DjangoObjectType):
         model = Link
 
 
-class Query(graphene.ObjectType):
-    links = graphene.List(LinkType)
+class VoteType(DjangoObjectType):
+    class Meta:
+        model = Vote
 
-    def resolve_links(self, info, **kwargs):
-        return Link.objects.all()
+
+class Query(graphene.ObjectType):
+    links = graphene.List(LinkType, search=graphene.String(), first=graphene.Int(), skip=graphene.Int())
+    votes = graphene.List(VoteType)
+
+    def resolve_links(self, info, search=None, first=None, skip=None, **kwargs):
+        qs = Link.objects.all()
+
+        if search:
+            filter = (
+                Q(url__icontains=search) |
+                Q(description__icontains=search)
+            )
+            qs = qs.filter(filter)
+
+        if skip:
+            qs = qs[skip:]
+
+        if first:
+            qs = qs[:first]
+
+        return qs
+    
+    def resolve_votes(self, info, **kwargs):
+        return Vote.objects.all()
+
     
 # Mutation class
 class CreateLink(graphene.Mutation):
     id = graphene.Int()
     url = graphene.String()
     description = graphene.String()
+    posted_by = graphene.Field(UserType)
 
     # Data to send to server
     class Arguments:
@@ -27,15 +57,44 @@ class CreateLink(graphene.Mutation):
         description = graphene.String()
 
     def mutate(self, info, url, description):
-        link = Link(url=url, description=description)
+        user = info.context.user or None
+
+        link = Link(url=url, description=description, posted_by=user)
         link.save()
 
         return CreateLink(
             id=link.id,
             url=link.url,
             description=link.description,
+            posted_by=link.posted_by,
         )
     
+class CreateVote(graphene.Mutation):
+    user = graphene.Field(UserType)
+    link = graphene.Field(LinkType)
+
+    class Arguments:
+        link_id = graphene.Int()
+
+    def mutate(self, info, link_id):
+        user = info.context.user
+        if user.is_anonymous:
+            raise GraphQLError('You must be logged to vote!')
+
+        link = Link.objects.filter(id=link_id).first()
+        if not link:
+            raise Exception('Invalid Link!')
+
+        Vote.objects.create(
+            user=user,
+            link=link,
+        )
+
+        return CreateVote(user=user, link=link)
+    
+    
+
 # Create mutation class
 class Mutation(graphene.ObjectType):
     create_link = CreateLink.Field()
+    create_vote = CreateVote.Field()
